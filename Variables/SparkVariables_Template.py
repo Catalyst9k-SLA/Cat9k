67e37a6885547fe45e0c34ef1a7be23c38876fff
AntoineOrsoni <ant.orsoni@gmail.com>
2018-04-04 10:01:18 +0200

    Adding Variables file

---
+++ Variables/SparkVariables_Template.py
@@ -0,0 +1,12 @@
+#####
+# File where we store the variable. This file is the template. The other file (with the values) will not be sent to github.
+#####
+
+# room ID
+
+roomID_SoftwareProject = ""
+
+# bearer
+
+bearer_Antoine = ""
+bearer_Bot = ""
