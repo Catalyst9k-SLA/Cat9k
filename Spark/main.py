e58d8129053a90926b6ecfc5eecdcb25324e346f
AntoineOrsoni <ant.orsoni@gmail.com>
2018-04-04 10:29:38 +0200

    First try to send a triggered message from the Cat9k

--- Spark/main.py
+++ Spark/main.py
@@ -1,3 +1,13 @@
+import sys
+sys.path.insert(0, '../Variables/')
+from SparkVariables import *
 from SparkFunctions import *
 
-postMessage("Y2lzY29zcGFyazovL3VzL1JPT00vM2IzYjUyOTAtMTMxMy0xMWU4LWJlY2EtN2RlZGYwNWYxOTFh", "test from python")
+#postMessage("Y2lzY29zcGFyazovL3VzL1JPT00vM2IzYjUyOTAtMTMxMy0xMWU4LWJlY2EtN2RlZGYwNWYxOTFh", "test from python")
+
+print("room ID :" + roomID_SoftwareProject)
+
+resp = post_message("Test - from the Cat9k", roomID_SoftwareProject, bearer_Bot)
+
+print("resp = " + resp.text)
+
