ec30c3043a713fb7b7687da0d1eb0e4895406456
AntoineOrsoni <ant.orsoni@gmail.com>
2018-04-04 11:26:35 +0200

    fixing directory

--- Spark/main.py
+++ Spark/main.py
@@ -1,5 +1,13 @@
+# Importing the variable file in the dir Variable
 import sys
-sys.path.insert(1, '../Variables/')
+import os
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+dirParent = os.path.dirname(currentdir)
+dirVariable = dirParent + "/Variables"
+sys.path.insert(0, dirVariable)
+
+
 from SparkVariables import *
 from SparkFunctions import *
 
