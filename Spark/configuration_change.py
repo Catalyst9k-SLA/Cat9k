a7a71001b6a1e39427f52b5ee720028a9bbfbe76
AntoineOrsoni <ant.orsoni@gmail.com>
2018-04-04 12:29:28 +0200

    adding configuration change

---
+++ Spark/configuration_change.py
@@ -0,0 +1,19 @@
+# Importing the variable file in the dir Variable
+import sys
+import os
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+dirParent = os.path.dirname(currentdir)
+dirVariable = dirParent + "/Variables"
+sys.path.insert(0, dirVariable)
+
+
+from SparkVariables import *
+from SparkFunctions import *
+
+print("room ID :" + roomID_SoftwareProject)
+
+resp = post_message("Configuration has changed", roomID_SoftwareProject, bearer_Bot)
+
+print("resp = " + resp.text)
+
