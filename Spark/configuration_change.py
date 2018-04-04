a1d31c1e714d75f66022eeda2b1da030acdba454
AntoineOrsoni <ant.orsoni@gmail.com>
2018-04-04 12:38:13 +0200

    configuration changed, adding user who did it

--- Spark/configuration_change.py
+++ Spark/configuration_change.py
@@ -7,13 +7,22 @@ dirParent = os.path.dirname(currentdir)
 dirVariable = dirParent + "/Variables"
 sys.path.insert(0, dirVariable)
 
-
+from argparse import ArgumentParser
 from SparkVariables import *
 from SparkFunctions import *
 
+
+parser = ArgumentParser("Spark Check In")
+    parser.add_argument(
+        "-w", "--who", help="Spark Authentication Token", required=True
+    )
+
+    args = parser.parse_args()
+    who = args.who
+
 print("room ID :" + roomID_SoftwareProject)
 
-resp = post_message("Configuration has changed", roomID_SoftwareProject, bearer_Bot)
+resp = post_message("Configuration has changed. Made by " + who, roomID_SoftwareProject, bearer_Bot)
 
 print("resp = " + resp.text)
 
