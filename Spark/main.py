924f0f03b5cd37ae7f7ae25512328f75e8567f70
AntoineOrsoni <ant.orsoni@gmail.com>
2018-04-04 11:05:22 +0200

    test

--- Spark/main.py
+++ Spark/main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(1, '../Variables/')
-from SparkVariables import *
+from Variables.SparkVariables import *
 from SparkFunctions import *
 
 #postMessage("Y2lzY29zcGFyazovL3VzL1JPT00vM2IzYjUyOTAtMTMxMy0xMWU4LWJlY2EtN2RlZGYwNWYxOTFh", "test from python")
