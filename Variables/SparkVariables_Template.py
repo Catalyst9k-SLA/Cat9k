8bb4da5f0916f2ce03ca470acc55d0af618b519a
AntoineOrsoni <ant.orsoni@gmail.com>
2018-04-06 10:52:11 +0200

    Update sparkvariables_template

--- Variables/SparkVariables_Template.py
+++ Variables/SparkVariables_Template.py
@@ -4,13 +4,18 @@
 
 # room ID
 
-roomID_SoftwareProject = "Y2lzY29zcGFyazovL3VzL1JPT00vM2IzYjUyOTAtMTMxMy0xMWU4LWJlY2EtN2RlZGYwNWYxOTFh"
+roomID_SoftwareProject = ""
 
 # bearer
 
-bearer_Antoine = "NTNlNmY1NjQtNTM4Ny00OThlLWFkMzAtZTc4YjkyYzAyZGI0OWE1ODVmY2ItODIx"
-bearer_Bot = "ZTRlYTY5YjQtODMxNC00NDJhLWFmM2YtZmU5OGI0MDAxN2Y1MjEyNDg0YzYtZmFi"
+bearer_Antoine = ""
+bearer_Bot = ""
 
-webhook_URL = "https://2f7a969f.ngrok.io/webhook"
-webhook_name = "webhook"
-peopleID_Kitty = "Y2lzY29zcGFyazovL3VzL1BFT1BMRS80N2ZjODNjMi01MzdlLTRiMTMtOTYzYy0xNTM3MTc4MTAzZmI"
+webhook_URL = ""
+webhook_name = ""
+peopleID_Kitty = ""
+
+# bot
+
+botName_Kitty = ""
+
