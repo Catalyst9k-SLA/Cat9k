b167928122342032b5ad0fea3802320cd794551d
sajustin <sajustin@cisco.com>
2018-04-06 10:15:13 +0200

    cli

--- Spark/saveconfig.py
+++ Spark/saveconfig.py
@@ -12,3 +12,6 @@ import cli
 print "\n\n *** Printing show cmd with 'executep' function *** \n\n"
 cli.executep('show ip interface brief')
 
+print "\n\n *** Printing show cmd with 'execute' function *** \n\n"
+output= cli.execute('copy start-up config running-config)
+print (output)
