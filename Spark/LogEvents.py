b36a48f86daec3e93f2de25c312977b372139c95
sajustin <sajustin@cisco.com>
2018-04-23 10:06:50 +0200

    Log all triggered events

---
+++ Spark/LogEvents.py
@@ -0,0 +1,22 @@
+# Importing the variable file in the dir Variable
+import sys
+import os
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+dirParent = os.path.dirname(currentdir)
+dirVariable = dirParent + "/Variables"
+sys.path.insert(0, dirVariable)
+
+from argparse import ArgumentParser
+from SparkVariables import *
+from SparkFunctions import *
+
+arguments = sys.argv
+print("arguments =" + str(arguments))
+
+print("room ID :" + roomID_SoftwareProject)
+
+resp = post_message_markdown("event triggered ! \n", roomID_SoftwareProject, bearer_Bot)
+resp = post_message_markdown(arguments, roomID_SoftwareProject, bearer_Bot)
+
+print("resp = " + resp.text)
